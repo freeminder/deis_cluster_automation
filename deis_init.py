f0e9c6ab28c382baed3210942337db57ab9aa8c0
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2015-01-24 22:24:55 +0700

    deis_init.py updated.

--- deis_init.py
+++ deis_init.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 import dosa
 from subprocess import call
-from random import randint
-import re
-import massedit
-# generate and push new cluster id
-cluster_id = call(["curl https://discovery.etcd.io/new"])
-
-filenames = ['/etc/apt/sources.list']
-massedit.edit_files(filenames, ["re.sub(r'^# deb', 'deb', line)"], dry_run=True)
-
-cluster_string = 'https://discovery.etcd.io/b827b6e8fa78993a03e04944d834db45'
-re.sub(r'(:[a-z]+)', r'b827b6e8fa78993a03e04944d834db45\1cluster_id', cluster_string)
-call(["git add cloud-config"])
-call(["git commit -m 'cloud-config updated.'"])
-call(["git push"])
+import urllib2
+import fileinput
+import shutil
+import os
 
 API_KEY = 'ad64ddd15bf37aa7157b30b3eada5d0d254936ae11d30e6c8ae386c9d2803d1c'
 dosa.set_debug()  # enables debug logs
 client = dosa.Client(api_key=API_KEY)
 
-# Droplets
-# client.droplets.list()
+
+# Remove the old config file if exists and copy new one
+if os.path.isfile('./cloud-config'): os.remove('./cloud-config')
+shutil.copyfile('./cloud-config.tmpl', './cloud-config')
+
+# Generate, replace and push new cluster id
+cluster_id = urllib2.urlopen("https://discovery.etcd.io/new").read()
+for line in fileinput.input("cloud-config", inplace=True):
+    print(line.replace("https://discovery.etcd.io/b827b6e8fa78993a03e04944d834db45", cluster_id))
+
+call(["git", "add", "cloud-config"])
+call(["git", "commit", "-m", "'cloud-config updated.'"])
+call(["git", "push"])
+
+
+# Create new droplet
 vm_gen_num = randint(2,99)
 status, result = client.droplets.create(name='Dmitry.CoreOS.test' + vm_gen_num, region='nyc3',\
     size='4gb', image='coreos-stable', private_networking='true', ssh_keys=['534374'])
@@ -30,9 +34,23 @@ new_droplet = client.Droplet(new_droplet_id)
 pub_ip = new_droplet.ip_addresses()[1]
 call(["/usr/bin/ssh", "core@" + pub_ip, "sudo /usr/bin/coreos-cloudinit --from-url=https://raw.githubusercontent.com/freeminder/deis_cluster_automation/master/cloud-config"])
 
-# print(new_droplet.info())
-## shortcuts
-# new_droplet.status()
+# Configure DEIS cluster
+new_droplet = client.Droplet(new_droplet_id)
+pub_ip = new_droplet.ip_addresses()[1]
+call(["/usr/bin/ssh", "core@" + pub_ip, "sudo /usr/bin/coreos-cloudinit --from-url=https://raw.githubusercontent.com/freeminder/deis_cluster_automation/master/cloud-config"])
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # SSH Keys
@@ -43,5 +61,8 @@ call(["/usr/bin/ssh", "core@" + pub_ip, "sudo /usr/bin/coreos-cloudinit --from-u
 # Images
 # client.images.list()
 
-
+## shortcuts
+# new_droplet.status()
+# client.droplets.list()
+# print(new_droplet.info())
 # client.droplets.delete(new_droplet_id)
